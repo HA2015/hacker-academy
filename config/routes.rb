858b257c0f8cb39e5f698001305537a724faa0d3
Alan Daniels <alan.daniels@hotmail.com>
2015-12-05 15:26:21 -0500

    Add links page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   root                      'static_pages#home'
   get 'about'      =>       'static_pages#about'
+  get 'links'      =>       'static_pages#links'
   
   get 'signup'     =>       'users#new'
 
